4d2decb47b47969b410947381b2acbc1deddc90d
Abhisht Pandey <abhishtpandey@Abhisht-MacBook-Air.local>
2021-02-19 01:28:53 +0530

    Adding Platform

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var platform;
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -12,7 +13,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -54,4 +55,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
